af00aa28c7e2d53caf70982f96edca3f73ed6bbb
IgorVianadF <igorv02004@gmail.com>
2023-09-19 22:24:21 -0300

    concat, imperativo&declarativo, flatmap

---
+++ JsExercicios/Aulas/Arrays/concat.js
@@ -0,0 +1,6 @@
+const frutas = ['pera','lixia','banana'];
+const vegetais = ['cebola','pimentao','quiabo'];
+const alimentos = frutas.concat(vegetais,'pão');
+console.log(alimentos);
+
+console.log([].concat([1,2],[3,4],5,[[6,7]]));
